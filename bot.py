45faf333257bd0ac731c03156617df68636fd7fc
AlexMaximenko <maksimenko.ae@phystech.edu>
2020-05-21 14:28:07 +0300

    added exception handling and the forecast command

--- bot.py
+++ bot.py
@@ -1,38 +1,158 @@
 import telebot
+import pyowm
+import pyowm.exceptions
 import config
 import time as tm
-from weather import get_weather
+import datetime
+from datetime import date
+from datetime import timedelta
+from weather import *
 
-bot = telebot.TeleBot(config.BOT_TOKEN)
+
+bot = telebot.TeleBot(config.BOT_TOKEN, threaded=False)
 
 @bot.message_handler(commands=['start'])
 def starting(message):
     start_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
                                                      one_time_keyboard=False)
-    start_markup.row('/start', '/hide', '/weather')
+    start_markup.row('/forecast', '/weather', '/hide')
     bot.send_message(message.chat.id, "Привет!\n")
     bot.send_message(message.chat.id, "Чтобы скрыть клавиатуру, нажмите \hide\n")
     bot.send_message(message.chat.id,
-                     "Чтобы узнать погоду, нажмите \weather",
+                     "Чтобы узнать прогноз на определенный день, нажмите \forecast\n")
+    bot.send_message(message.chat.id,
+                     "Чтобы узнать текущую погоду, нажмите \weather",
                      reply_markup=start_markup)
 
 
 @bot.message_handler(commands=['hide'])
 def hiding(message):
     bot.send_message(message.chat.id,
-                     '...',
+                     'Клавиатура спрятана',
                      reply_markup=telebot.types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(commands=['weather'])
-def weather_command(message):
+def weather_city_receive(message):
     city_request = bot.send_message(message.chat.id,
-                                    'Введите город или страну, в которой хотите узнать погоду')
+                                    'Введите город, в котором хотите узнать погоду')
     bot.register_next_step_handler(city_request, send_weather)
 
+
 def send_weather(message):
-    answer = get_weather(message.text)
-    bot.send_message(message.chat.id, answer)
+    try:
+        get_weather(message.text)
+    except pyowm.exceptions.api_response_error.NotFoundError:
+        bot.send_message(message.chat.id,
+                         '❌Город не найден, убедитесь в правильности написания')
+    bot.send_message(message.chat.id, get_weather(message.text))
+
+
+@bot.message_handler(commands=['forecast'])
+def forecast_day_request(message):
+    day_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    today = date.today()
+    possible_days = []
+
+    for i in range(1, 6):
+        delta = timedelta(days = i)
+        day = months[(today+delta).month] + ', '+(today + delta).strftime(("%d"))
+        possible_days.append(day)
+
+    day_markup.row(possible_days[0],
+                   possible_days[1],
+                   possible_days[2],
+                   possible_days[3],
+                   possible_days[4])
+    request =  bot.send_message(message.chat.id,
+                                    'В какой день вы хотите узнать погоду?',
+                                    reply_markup=day_markup)
+
+    bot.register_next_step_handler(request, forecast_city_request)
+
+def forecast_city_request(message):
+    today = date.today()
+    possible_days = []
+
+    for i in range(1, 6):
+        delta = timedelta(days = i)
+        day = months[(today+delta).month] + ', '+(today + delta).strftime(("%d"))
+        possible_days.append(day)
+
+    if not any(message.text in day for day in possible_days):
+        bot.send_message(message.chat.id,
+                         "❌Выбирайте день только из предложенных ниже!",
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+    else:
+        send_forecast = {possible_days[0]: send_forecast_1,
+                        possible_days[1]: send_forecast_2,
+                        possible_days[2]: send_forecast_3,
+                        possible_days[3]: send_forecast_4,
+                        possible_days[4]: send_forecast_5}
+        next_handler = send_forecast.get(message.text)
+        request = bot.send_message(message.chat.id,
+                                        'Введите город, в котором хотите узнать погоду')
+        bot.register_next_step_handler(request, next_handler)
+
+
+def send_forecast_1(message):
+    try:
+        get_forecast(message.text, 1)
+    except pyowm.exceptions.api_response_error.NotFoundError:
+        bot.send_message(message.chat.id,
+                         '❌Город не найден, убедитесь в правильности написания',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+
+    bot.send_message(message.chat.id,
+                     get_forecast(city=message.text, days=1),
+                     reply_markup=telebot.types.ReplyKeyboardRemove())
+
+def send_forecast_2(message):
+    try:
+        get_forecast(message.text, 2)
+    except pyowm.exceptions.api_response_error.NotFoundError:
+        bot.send_message(message.chat.id,
+                         '❌Город не найден, убедитесь в правильности написания',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+
+    bot.send_message(message.chat.id,
+                     get_forecast(city=message.text, days=2),
+                     reply_markup=telebot.types.ReplyKeyboardRemove())
+
+def send_forecast_3(message):
+    try:
+        get_forecast(city=message.text, days=3)
+    except pyowm.exceptions.api_response_error.NotFoundError:
+        bot.send_message(message.chat.id,
+                         '❌Город не найден, убедитесь в правильности написания',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+    bot.send_message(message.chat.id,
+                     get_forecast(city=message.text, days=3),
+                     reply_markup=telebot.types.ReplyKeyboardRemove())
+
+def send_forecast_4(message):
+    try:
+        get_forecast(message.text, 4)
+    except pyowm.exceptions.api_response_error.NotFoundError:
+        bot.send_message(message.chat.id,
+                         '❌Город не найден, убедитесь в правильности написания',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+
+    bot.send_message(message.chat.id,
+                     get_forecast(city=message.text, days=4),
+                     reply_markup=telebot.types.ReplyKeyboardRemove())
+
+def send_forecast_5(message):
+    try:
+        get_forecast(message.text, 5)
+    except pyowm.exceptions.api_response_error.NotFoundError:
+        bot.send_message(message.chat.id,
+                         '❌Город не найден, убедитесь в правильности написания',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+
+    bot.send_message(message.chat.id,
+                     get_forecast(city=message.text, days=5),
+                     reply_markup=telebot.types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(content_types=['text'])
@@ -42,4 +162,8 @@ def send_text(message):
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'Пока')
 
-bot.polling(none_stop=True)
+while True:
+	try:
+		bot.infinity_polling(True)
+	except Exception:
+		tm.sleep(1)
--- weather.py
+++ weather.py
@@ -1,7 +1,22 @@
 import pyowm
 from config import OWM_TOKEN
+from datetime import datetime
+from datetime import date
 
 owm = pyowm.OWM(OWM_TOKEN, language='ru')
+
+months = {1: 'Январь',
+          2: 'Февраль',
+          3: 'Март',
+          4: 'Апрель',
+          5: 'Май',
+          6: 'Июнь',
+          7: 'Июль',
+          8: 'Август',
+          9: 'Сентябрь',
+          10: 'Октябрь',
+          11: 'Ноябрь',
+          12: 'Декабрь'}
 
 def get_weather(city):
     observation = owm.weather_at_place(city)
@@ -12,3 +27,21 @@ def get_weather(city):
     humidity = weather.get_humidity()
     weather = f"В городе {city} сейчас {weather.get_detailed_status()} \nТемпература: {temperature} °C \nВетер: {wind} m/s \nОблачность: {clouds} % \nВлажность: {humidity} %"
     return weather
+
+def get_forecast(city, days):
+    observation = owm.weather_at_place(city)
+    forecast = owm.three_hours_forecast(city).get_forecast()
+    today = date.today()
+    start_date = date(today.year, today.month, today.day + days)
+    finish_date = date(today.year, today.month, today.day + days + 1)
+    answer = ''
+
+    for weather in forecast:
+        ref_date = weather.get_reference_time('date').date()
+        ref_time = weather.get_reference_time('date').time()
+        if ref_date < start_date or ref_date >= finish_date:
+            continue
+        temperature = weather.get_temperature('celsius')["temp"]
+        answer += f'В {ref_time.strftime("%H:%M")} будет {weather.get_detailed_status()}, температура - {temperature}\n'
+
+    return answer
